98769541c214afe055b569161939f315a51d0d8c
Kellon Ko <kellondavidko@gmail.com>
2022-10-06 15:15:06 -0700

    add winning combinations

--- script.js
+++ script.js
@@ -10,9 +10,21 @@ const player = (sign) => {
 };
 
 // Game board
-const game = () => {
-  const field = ["", "", "", "", "", "", "", "", ""];
+const gameBoard = () => {
+  const gameField = document.querySelectorAll("data-cell");
 };
+
+// Check winner
+const winCombos = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
 
 // Restart
 const restartBtn = document.getElementById("restart");
