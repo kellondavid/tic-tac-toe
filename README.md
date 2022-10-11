c4f813ad05479022e4d71dbe1fd0886046c537fa
Kellon Ko <kellondavidko@gmail.com>
2022-10-11 16:04:00 -0700

    add declaration of winner, update readme

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# tic-tac-toe
+LIVE: https://kellondavid.github.io/tic-tac-toe/
+
+Odin Project's Tic Tac Toe Game Assignment
+
+General Thoughts: Assignment introduced us to module patterns and factory functions, and I decided to use module patterns for my gameboard. Creating the game itself wasn't too difficult, but figuring out and writing the logic to check for game winner took quite a while. I had to do some research on Array.prototype.some(), but I was able to write the code after doing so! Additionally, I think there is a better way to code this game, because I don't think I should be relying on UI to hold the game information. In the future, I will probably come back to this project and refactor the code to rely on the state/data (e.g., `const gameBoard = ["X", "X", "O", "", "", "O", "", "", "X"];`), and make the UI to update based on the array.
+
+What I learned:
+
+- Factory functions and module patterns
+- Array.prototype.some();
+- How to remove outside borders in CSS
--- script.js
+++ script.js
@@ -1,42 +1,59 @@
 // Globals
-let playerDisplay = document.querySelector(".playerTurn")
-playerDisplay.textContent = "Player X's turn!"
+let playerDisplay = document.querySelector(".playerTurn");
+playerDisplay.textContent = "Player X's turn!";
+
 // Game board
 const gameBoard = (() => {
   const gameField = document.querySelectorAll(".squares");
   gameField.forEach((square) => {
     square.addEventListener("click", handleClick, { once: true });
   });
-  let currentPlayer = "x"
-  
+  let currentPlayer = "X";
+  let prevPlayer = "O";
+
   function handleClick(e) {
-    const square = e.target
-    if (currentPlayer === "x") {
-      currentPlayer = "o"
-      square.textContent = "X"
-      square.classList.add("xClass")
-      playerDisplay.textContent = "Player O's turn!"
-    }else if (currentPlayer === "o") {
-      currentPlayer = "x"
-      square.textContent = "O"
-      square.classList.add("oClass")
-      playerDisplay.textContent = "Player X's turn!"
+    const square = e.target;
+    if (currentPlayer === "X") {
+      currentPlayer = "O";
+      prevPlayer = "X";
+      square.textContent = "X";
+      square.classList.add(currentPlayer);
+      playerDisplay.textContent = "Player O's turn!";
+    } else if (currentPlayer === "O") {
+      currentPlayer = "X";
+      prevPlayer = "O";
+      square.textContent = "O";
+      square.classList.add(currentPlayer);
+      playerDisplay.textContent = "Player X's turn!";
+    }
+    if (checkWin(currentPlayer)) {
+      playerDisplay.textContent = "Player " + prevPlayer + " won!"
+      gameField.forEach((square) => {
+        square.removeEventListener("click", handleClick);
+      })
     }
   }
+  
+  // Check winner
+  const winCombos = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  function checkWin(currentPlayer) {
+    return winCombos.some((combinations) => {
+      return combinations.every((index) => {
+        return gameField[index].classList.contains(currentPlayer);
+      });
+    });
+  }
 })();
-
-// Check winner
-const winCombos = [
-  [0, 1, 2],
-  [3, 4, 5],
-  [6, 7, 8],
-  [0, 3, 6],
-  [1, 4, 7],
-  [2, 5, 8],
-  [0, 4, 8],
-  [2, 4, 6],
-];
-
 
 // Restart
 const restartBtn = document.getElementById("restart");
